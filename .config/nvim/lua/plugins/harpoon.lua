45f2df5481982d9c4d5836aefa2dc61ed022c062
kensqie <mail@kensqie.net>
2025-07-06 18:23:40 +0800

    feat(nvim): add harpoon

---
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -0,0 +1,34 @@
+return {
+	"ThePrimeagen/harpoon",
+	branch = "harpoon2",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		local harpoon = require('harpoon')
+		local conf = require("telescope.config").values
+
+		harpoon:setup({})
+
+		local function toggle_telescope(harpoon_files)
+			local file_paths = {}
+			for _, item in ipairs(harpoon_files.items) do
+				table.insert(file_paths, item.value)
+			end
+
+			require("telescope.pickers").new({}, {
+				prompt_title = "Harpoon",
+				finder = require("telescope.finders").new_table({
+					results = file_paths,
+				}),
+				previewer = conf.file_previewer({}),
+				sorter = conf.generic_sorter({}),
+			}):find()
+		end
+
+		vim.keymap.set("n", "<leader>hl", function() toggle_telescope(harpoon:list()) end,
+			{ desc = "Harpoon list" })
+		vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end, { desc = "Harpoon add" })
+		vim.keymap.set("n", "<leader>hd", function() harpoon:list():remove() end, { desc = "Harpoon delete" })
+		vim.keymap.set("n", "<leader>hn", function() harpoon:list():next() end, { desc = "Harpoon next" })
+		vim.keymap.set("n", "<leader>hp", function() harpoon:list():prev() end, { desc = "Harpoon prev" })
+	end
+}
