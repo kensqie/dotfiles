985dd40bcbd1dd13c02d7330ebf158b627810569
kensqie <mail@kensqie.net>
2026-01-05 21:36:46 +0800

    fix(nvim): tree-sitter breaking changes

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -2,14 +2,27 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
 	config = function()
-		local treesitter = require("nvim-treesitter.configs")
-
-		treesitter.setup({
+		require("nvim-treesitter").setup({
 			ensure_installed = { "vim", "vimdoc", "comment", "lua", "c" },
 			sync_install = false,
 			auto_install = false,
 			indent = { enable = true },
-			highlight = { enable = true },
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+			},
+		})
+
+
+		vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+			callback = function(args)
+				local bufnr = args.buf
+				local ft = vim.bo[bufnr].filetype
+
+				if ft and ft ~= "" then
+					pcall(vim.treesitter.start, bufnr)
+				end
+			end,
 		})
 	end,
 }
