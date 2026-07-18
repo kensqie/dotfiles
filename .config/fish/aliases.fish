8122792ea040db1802b78c598807bf39d41a69ab
kensqie <mail@kensqie.net>
2026-07-18 15:07:04 +0800

    feat(fish): remove fish config

--- .config/fish/aliases.fish
+++
@@ -1,7 +0,0 @@
-alias ls="ls -CF -v --color=auto --group-directories-first"
-
-if type eza > /dev/null 2>&1
-	alias lt="eza --group-directories-first -T"
-	alias ll="eza --group-directories-first -l"
-	alias la="ll -a"
-end
